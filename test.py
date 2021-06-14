38b0c2ad6fb7dee929c25de42779f916e69e9515
mbustillos329 <mbustillos329@gmail.com>
2021-06-14 00:22:09 -0500

    csv file

--- test.py
+++ test.py
@@ -4,3 +4,11 @@ import pickle
 
 app = Flask(__name__)
 model = pickle.load(open('pickle_ml.pickle', 'rb'))
+
+# @app.route('/')
+# def home():
+#     return render_template('index.html')
+
+# # predict button
+# @app.route('/predict', methods=['POST'])
+# # def predict():
