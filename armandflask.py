37e27fbfcbeeaa0af1f240920653c2a66a13dfe7
axfrancois123 <74634280+axfrancois123@users.noreply.github.com>
2021-06-17 21:20:23 -0500

    Add files via upload

--- armandflask.py
+++ armandflask.py
@@ -5,58 +5,22 @@ from tensorflow.keras.applications.xception import (
 Xception, preprocess_input, decode_predictions)
 import numpy as np
 import os
+import json
+import pandas as pd
 
 app = Flask(__name__)
-
-# dic = {0 : 'Cat', 1 : 'Dog'}
 
 model = Xception(
     include_top=True,
     weights='imagenet')
 
-# # model.make_predict_function()
-
-# def predict(image_path):
-#     # """Use Xception to label image"""
-#     image_size = (299,299)
-#     img = image.load_img(image_path, target_size=image_size)
-#     x = image.img_to_array(img)
-#     x = np.expand_dims(x, axis=0)
-#     x = preprocess_input(x)
-#     predictions = model.predict(x)
-#     results = decode_predictions(predictions, top=3)[0]
-#     return jsonify(results)
-
-
-# routes
-@app.route("/", methods=['GET'])
-def main():
-	return render_template("index.html")
-
-@app.route("/about")
-def about_page():
-	return "YEAHHHHHHHHH!!!!!!! "
-
-@app.route("/submit", methods = ['GET'])
-def get_output():
-	if request.method == 'POST':
-		img = request.files['my_image']
-
-		image_path = "data/" + img.filename	
-        
-		
-
-		p = predict(image_path)
-
-	return render_template("index.html", prediction = p, image_path = image_path)
-
 # Jeffs Help
-@app.route("/predict/<image_path>")
-def predict(image_path):
+
+@app.route("/predict")
+def predict():
     # """Use Xception to label image"""
-
-    image_path = os.path.join("..", "project3", "data", "1.jpg")
-
+    image_path = request.args.get("filename")
+    print(image_path)
     image_size = (299,299)
     img = image.load_img(image_path, target_size=image_size)
     x = image.img_to_array(img)
@@ -64,16 +28,7 @@ def predict(image_path):
     x = preprocess_input(x)
     predictions = model.predict(x)
     results = decode_predictions(predictions, top=3)[0]
-
-    return jsonify(results)
-
-
-    
-
-    
-
-
-
-if __name__ =='__main__':
-	#app.debug = True
-	app.run(debug = True)
+    df = pd.DataFrame(results, columns=["id", "prediction", "probability"])
+    new_dict = df.to_dict(orient="records")
+ 
+    return jsonify(new_dict)
