ca12f3cdc6468a830a4136a1dfdc412f11d26f27
Jennifer Reyna <jennifer.m.reyna@outlook.com>
2021-06-17 19:33:40 -0500

    change on click

--- webpage.js
+++ webpage.js
@@ -3,10 +3,17 @@
 ///////  D3 FILE FOR REDERING PET PHOTOS //////////
 
 
-d3.select("#selPets").on("click", function(){
+// d3.select("#selPets").on("click", function(){
 
-    var pets = changepets(d3.select('#selPets option:checked').text())
-    
-})
+//     var pets = changepets(d3.select('#selPets option:checked').text())
 
-function changepets(petoption){console.log(petoption)}
+
+
+// })
+
+function changepets(petoption){
+    console.log("petoption",petoption)
+    d3.select("#petpic").attr("src", petoption);
+
+}
+
