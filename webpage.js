5880c1e24693fda92bbfba8fa7bf5316c93f3434
Jennifer Reyna <jennifer.m.reyna@outlook.com>
2021-06-19 01:36:39 -0500

    webpage links

--- webpage.js
+++ webpage.js
@@ -2,18 +2,14 @@
 
 ///////  D3 FILE FOR REDERING PET PHOTOS //////////
 
-
-// d3.select("#selPets").on("click", function(){
-
-//     var pets = changepets(d3.select('#selPets option:checked').text())
-
-
-
-// })
-
 function changepets(petoption){
     console.log("petoption",petoption)
     d3.select("#petpic").attr("src", petoption);
 
 }
+// function setpredictions(mloption){
+//     console.log("mloption",mloption)
+//     d3.select("#mlpred").attr("div", mloption);
+// }
+
 
